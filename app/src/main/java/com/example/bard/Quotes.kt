9a1157e389068dd228b85bb726ca82d1b7af59f6
Gareth Parker <blackfireweb@gmail.com>
2019-08-27 15:08:06 +0100

    New Smashmouth Quote

--- app/src/main/java/com/example/bard/Quotes.kt
+++ app/src/main/java/com/example/bard/Quotes.kt
@@ -2,6 +2,7 @@ package com.example.bard
 
 class Quotes {
     private val quotes = listOf(
+        Quote("All Star", "Hey now, you're a warrior, \r\nGet your sword out, go slay. \r\nHey now, you're a killer. \r\nGet your Orc on, go flay. \r\nAll that splatters is blood. \r\nTurn the bastards insides into a flood"),
         Quote("Hey Jude", "Hey Jane. \r\nDon't make it bad. \r\nTake a live monster, and it deader. \r\nRemember, to stab into it's heart. \r\nThen you can start, to make it deader"),
         Quote("Let it Be", "When I find myself in times of trouble, \r\nInvandra appears to me. \r\nSinging words of healing: Let 'em heal"),
         Quote("Baby Got Back", "I like big rolls and I cannot lie. \r\nYou other brothers can't deny. \r\nWhen a dice rolls in with an itty bitty spin \r\nAnd a 20 on it's face you get SPRUNG!")
